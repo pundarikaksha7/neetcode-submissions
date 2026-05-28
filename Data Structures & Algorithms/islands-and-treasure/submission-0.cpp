dfbf1af7eb13ae19cdac91403166a3aa5c101ba3
Pundarikaksha Mohanty <98964611+pundarikaksha7@users.noreply.github.com>
2026-05-28 11:47:54 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    void dj(vector<vector<int>>&grid,
+    int i, int j){
+        //i and j are the indices of a 0
+        // have to do djisktra assuming this is the source
+
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,
+        greater<pair<int,pair<int,int>>>> pq;
+        int m = grid.size();
+        int n = grid[0].size();
+        // node i j, dist
+
+        pq.push({0,{i,j}});
+
+        vector<int>dirs{-1,0,1};
+
+        while(!pq.empty()){
+            int nodei = pq.top().second.first;
+            int nodej = pq.top().second.second;
+            int dist = pq.top().first;
+
+            pq.pop();
+            for(auto x:dirs){
+                for(auto y:dirs){
+                    if(abs(x)==abs(y))continue;
+                    int movex = nodei+x;
+                    int movey = nodej+y;
+
+                    if(movex < 0 || movey<0 || 
+                    movex>=m || movey>=n) continue;
+                    int newgrid = grid[movex][movey];
+
+                    if(newgrid !=-1 && newgrid>1+dist){
+                        grid[movex][movey] = 1+dist;
+                        pq.push({1+dist,{movex,movey}});
+                    }
+                }
+            }
+            
+        }
+
+    }
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+
+        for(int i = 0;i<m; i++){
+            for(int j = 0;j<n;j++){
+                if(grid[i][j]==0){
+                    dj(grid,i,j);
+                }
+            }
+        }
+        
+    }
+};
