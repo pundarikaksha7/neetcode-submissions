ecd864a8c275aebca5471bd99d3a1e0115271b2c
Pundarikaksha Mohanty <98964611+pundarikaksha7@users.noreply.github.com>
2026-06-20 15:42:06 +0530

    Add: edit-distance - submission-3

---
+++ Data Structures & Algorithms/edit-distance/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int f (string &s, string &t, int i , int j,vector<vector<int>>&dp){
+        int m = s.length(), n = t.length();
+        
+        if(i==m) return n-j;
+        if(j==n) return m-i;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int insert = INT_MAX, del = INT_MAX, replace = INT_MAX, move = INT_MAX;
+        if(j<n && s[i]==t[j]) move = f(s,t,i+1,j+1,dp);
+        if(j<n)insert = 1+f(s,t,i,j+1,dp);
+        del = 1+f(s,t,i+1,j,dp);
+        if(j<n)replace = 1+f(s,t,i+1,j+1,dp);
+
+        return dp[i][j]=min({insert,del,replace,move});
+    }
+    int minDistance(string s, string t) {
+        int m = s.length(), n = t.length();
+        vector<vector<int>>dp1(m+1,vector<int>(n+1,-1));
+        vector<vector<int>>dp2(n+1,vector<int>(m+1,-1));
+        // return m>=n?f(s,t,0,0,dp1):f(t,s,0,0,dp2);
+        return f(s,t,0,0,dp1);
+
+        
+    }
+};
